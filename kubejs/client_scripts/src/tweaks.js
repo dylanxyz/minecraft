010f208f502abed060fb40a7307637c711ba3360
Jorge Pereira <jorge.brito.json@outlook.com>
2024-12-17 20:56:16 -0300

    update kubejs client scripts

--- kubejs/client_scripts/src/tweaks.js
+++ kubejs/client_scripts/src/tweaks.js
@@ -1,7 +1,15 @@
 BlockEvents.rightClicked((event) => {
     const offhandItem = event.getPlayer().getHeldItem("off_hand")
     const emptyMainHand = event.getPlayer().getHeldItem("main_hand").isEmpty()
+    const blockItem = event.getBlock().getItem()
 
-    if (emptyMainHand && Ingredient.of("#kubejs:torch_like").test(offhandItem))
+    if (!Ingredient.of("#kubejs:torch_like").test(offhandItem))
+        return
+
+    const isGate = Ingredient.of("#minecraft:fence_gates").test(blockItem)
+    const isDoor = Ingredient.of("#minecraft:doors").test(blockItem)
+    const isTrapDoor = Ingredient.of("#minecraft:trapdoors").test(blockItem)
+
+    if (!isDoor && !isTrapDoor && !isGate && emptyMainHand)
         event.cancel()
 })

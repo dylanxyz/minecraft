d680f8c9450c1e18b4dde9a97714d434a09c7ace
Jorge Pereira <jorge.brito.json@outlook.com>
2024-12-21 00:24:22 -0300

    Add client tweaks

--- kubejs/client_scripts/src/tweaks.js
+++ kubejs/client_scripts/src/tweaks.js
@@ -1,15 +1,15 @@
-BlockEvents.rightClicked((event) => {
-    const offhandItem = event.getPlayer().getHeldItem("off_hand")
-    const emptyMainHand = event.getPlayer().getHeldItem("main_hand").isEmpty()
-    const blockItem = event.getBlock().getItem()
+// BlockEvents.rightClicked((event) => {
+//     const offhandItem = event.getPlayer().getHeldItem("off_hand")
+//     const emptyMainHand = event.getPlayer().getHeldItem("main_hand").isEmpty()
+//     const blockItem = event.getBlock().getItem()
 
-    if (!Ingredient.of("#kubejs:torch_like").test(offhandItem))
-        return
+//     if (!Ingredient.of("#kubejs:torch_like").test(offhandItem))
+//         return
 
-    const isGate = Ingredient.of("#minecraft:fence_gates").test(blockItem)
-    const isDoor = Ingredient.of("#minecraft:doors").test(blockItem)
-    const isTrapDoor = Ingredient.of("#minecraft:trapdoors").test(blockItem)
+//     const isGate = Ingredient.of("#minecraft:fence_gates").test(blockItem)
+//     const isDoor = Ingredient.of("#minecraft:doors").test(blockItem)
+//     const isTrapDoor = Ingredient.of("#minecraft:trapdoors").test(blockItem)
 
-    if (!isDoor && !isTrapDoor && !isGate && emptyMainHand)
-        event.cancel()
-})
+//     if (!isDoor && !isTrapDoor && !isGate && emptyMainHand)
+//         event.cancel()
+// })
